485e8c42a4eb17d294ad78646441d021483c9386
pert5432 <p3tr.kada@gmail.com>
2024-12-28 00:35:16 +0100

    select builder prototype

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { Client } from "pg";
+
+class Column {
+  constructor(name: string, fieldName: string) {
+    this.name = name;
+    this.fieldName = fieldName;
+  }
+
+  name: string;
+
+  fieldName: string;
+}
+
+class Entity {
+  constructor(name: string, className: string, columns: Column[]) {
+    this.name = name;
+    this.className = className;
+    this.columns = columns;
+  }
+
+  name: string;
+
+  className: string;
+
+  columns: Column[];
+}
+
+const buildSelect = (e: Entity): string => {
+  const targets = e.columns
+    .map((c) => `${c.name} AS "${e.className}.${c.fieldName}"`)
+    .join(", ");
+
+  return `SELECT ${targets} FROM ${e.name}`;
+};
+
+const main = async () => {
+  const client = new Client("postgres://petr@localhost:5437/tygress");
+  await client.connect();
+
+  const users = new Entity("users", "Users", [
+    new Column("id", "ID"),
+    new Column("username", "Username"),
+    new Column("full_name", "FullName"),
+  ]);
+
+  const sql = buildSelect(users);
+
+  console.log(sql);
+
+  console.log((await client.query(sql)).rows);
+};
+
+main();
