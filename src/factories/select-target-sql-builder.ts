271b29282476fa60a6cf0bc98ba80d1f8f13fb4b
pert5432 <p3tr.kada@gmail.com>
2025-02-19 21:48:40 +0100

    ✂️

--- src/factories/select-target-sql-builder.ts
+++ src/factories/select-target-sql-builder.ts
@@ -1,32 +1,11 @@
-import { ColumnMetadata } from "../metadata";
 import {
   ColumnIdentifierSqlBuilder,
   ColumnSelectTargetSqlBuilder,
   SqlSelectTargetSqlBuilder,
 } from "../sql-builders";
 import { NamedParams } from "../types/named-params";
-import { ColumnIdentifierSqlBuilderFactory } from "./column-identifier";
 
 export abstract class SelectTargetSqlBuilderFactory {
-  static createColumn(
-    alias: string,
-    column: ColumnMetadata,
-    as: string
-  ): ColumnSelectTargetSqlBuilder {
-    const e = new ColumnSelectTargetSqlBuilder();
-
-    e.columnIdentifier = ColumnIdentifierSqlBuilderFactory.createColumnMeta(
-      alias,
-      column
-    );
-    e.as = as;
-
-    e.nodeAlias = alias;
-    e.fieldName = column.fieldName;
-
-    return e;
-  }
-
   static createColumnIdentifier(
     columnIdentifier: ColumnIdentifierSqlBuilder,
     as: string,
