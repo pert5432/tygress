e167e6f108f2d74bf0ebc8693aea429cb2f676c8
pert5432 <p3tr.kada@gmail.com>
2025-04-20 17:46:33 +0200

    transaction control API

--- src/postgres-connection.ts
+++ src/postgres-connection.ts
@@ -5,6 +5,30 @@ export class PostgresConnection {
 
   public query = this.$client.query;
 
+  public async begin(): Promise<void> {
+    await this.query("BEGIN;");
+  }
+
+  public async commit(): Promise<void> {
+    await this.query("COMMIT;");
+  }
+
+  public async rollback(): Promise<void> {
+    await this.query("ROLLBACK");
+  }
+
+  public async startTransaction(): Promise<void> {
+    await this.begin();
+  }
+
+  public async commitTransaction(): Promise<void> {
+    await this.commit();
+  }
+
+  public async rollbackTransaction(): Promise<void> {
+    await this.rollback();
+  }
+
   public release(): void {
     this.$client.release();
   }
