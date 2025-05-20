9c94a4f9cb9a418bf560001c176d1d9ab8fd87a8
pert5432 <p3tr.kada@gmail.com>
2025-05-20 23:37:54 +0200

    export more types and enums

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { PostgresClient } from "./postgres-client";
+import { QueryLogLevel } from "./enums";
+import { AnEntity } from "./types";
 
+export { AnEntity };
 export { PostgresClient };
+export { QueryLogLevel };
 export * from "./api";
 export * from "./decorators";
+export * from "./types/connection-settings";
