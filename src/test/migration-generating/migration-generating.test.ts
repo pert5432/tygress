86b2a7f4f1d815f7b6d6267d414f91de6533c1a1
pert5432 <p3tr.kada@gmail.com>
2025-07-07 21:00:59 +0200

    more create table tests

--- src/test/migration-generating/migration-generating.test.ts
+++ src/test/migration-generating/migration-generating.test.ts
@@ -1,30 +1,42 @@
 import * as fs from "fs";
-import { afterAll, describe, expect, test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 import { getClient } from "./client";
-import { Members } from "./entities/members";
-import { Groups } from "./entities/groups";
 import { countMigrationFiles } from "./count-migration-files";
 import path from "path";
 import { validateLatestMigration } from "./validate-latest-migration";
+import { Members, Groups, Groups2, Members2 } from "./entities/";
 
 describe("migration generation", async () => {
   // Recreate migrations folder after tests finish
-  afterAll(() => {
+  beforeAll(async () => {
     fs.rmSync(path.join(__dirname, "migrations"), {
       recursive: true,
       force: true,
     });
 
     fs.mkdirSync(path.join(__dirname, "migrations"));
+
+    // Drop tables used for migration tests
+    await getClient([Members, Groups, Members2, Groups2]).query(
+      "DROP TABLE IF EXISTS members, groups, members2, groups2;"
+    );
   });
 
-  test("generates create statements", async () => {
-    const DB = getClient([Members, Groups]);
+  describe("generates create statements", async () => {
+    test("version 1", async () => {
+      await getClient([Members, Groups]).generateMigration("init");
 
-    await DB.generateMigration("init");
+      expect(countMigrationFiles()).toEqual(1);
 
-    expect(countMigrationFiles()).toEqual(1);
+      validateLatestMigration("init.migration");
+    });
 
-    validateLatestMigration("init.migration");
+    test("version 2", async () => {
+      await getClient([Members2, Groups2]).generateMigration("init2");
+
+      expect(countMigrationFiles()).toEqual(2);
+
+      validateLatestMigration("init2.migration");
+    });
   });
 });
