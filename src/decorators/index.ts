3784ef45e6683389e71595bc51a6bde0cd57cce4
pert5432 <p3tr.kada@gmail.com>
2025-07-30 19:18:11 +0200

    export decorator

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -3,3 +3,4 @@ export { ManyToOne } from "./many-to-one";
 export { OneToMany } from "./one-to-many";
 export { Table } from "./table";
 export { PrimaryKey } from "./primary-key";
+export { Index } from "./index-decorator";
