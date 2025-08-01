a04fe0b867f577f63ad49f8a179da08dace5497c
pert5432 <p3tr.kada@gmail.com>
2025-08-02 01:50:17 +0200

    comments

--- src/sql-builders/constant-builder.ts
+++ src/sql-builders/constant-builder.ts
@@ -1,9 +1,21 @@
 export abstract class ConstantBuilder {
+  /**
+   * Returns params that should be sent alongside the SQL statement
+   */
   public abstract get params(): any[];
 
+  /**
+   * Returns the provided value in the way it should appear in the SQL statement
+   *
+   * @param val value to be sent to SQL
+   */
   public abstract addConst(val: any): string;
 }
 
+/**
+ * Returns param placeholders $1, $2, $3...
+ * `params` returns the consts provided in `addConst` in the order they were provided in
+ */
 export class ParametrizedConstantBuilder extends ConstantBuilder {
   private _params: any[] = [];
 
